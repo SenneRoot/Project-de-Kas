aadbd769298d94cb52d842ce75f4ad30f721863e
SenneRoot <senner@hotmail.nl>
2016-05-12 19:29:55 +0200

    Add files via upload

--- LDR.ino
+++ LDR.ino
@@ -12,33 +12,13 @@ void loop()
 {
 	readSensor(sensorPin);
 	Monitor(sensorValue);
-	LichtSensor(ledPin, sensorValue, 500);
-	delay(100); 
-}
 
-static void ledon(int ledPin)
-{
-	digitalWrite(ledPin, HIGH);
-}
-
-static void ledoff(int ledPin)
-{
-	digitalWrite(ledPin, LOW);
 }
 
 static double readSensor(int sensorPin)
 {
 	sensorValue = analogRead(sensorPin);
 	return sensorValue;
-}
-
-static void LichtSensor(int ledPin, int sensorValue, int lichtwaarde)
-{
-	if (sensorValue < lichtwaarde) // setting a threshold value
-		digitalWrite(ledPin, HIGH); // turn LED ON
-
-	else
-		digitalWrite(ledPin, LOW); // turn LED OFF
 }
 
 static void Monitor(double sensorValue)
